9cdb614e9fcd2267a6921922f83a784dd595665e
Ricardo <ricardoinliner@yahoo.com>
2021-07-06 20:14:31 -0300

    updated basepath on gh-pages

--- client/next.config.js
+++ client/next.config.js
@@ -4,12 +4,13 @@ const withSass = require('sass')
 const withPlugins = require('next-compose-plugins');
 const isDev = process.env.NODE_ENV !== "production";
 
+const GH_PAGE_REPOSITORY = "addressbook-dapp"
 
 module.exports = withPlugins([
     withImages,
     withSass,
     {
         trailingSlash: true,
-        basePath: isDev ? "": "/tokenize-dapp",
-        assetPrefix: isDev ? "": "/tokenize-dapp/"    }
+        basePath: isDev ? "": `/${GH_PAGE_REPOSITORY}`,
+        assetPrefix: isDev ? "": `/${GH_PAGE_REPOSITORY}/`   }
 ])
