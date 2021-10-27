c17590da4957d9ca0ea6b33b5da66fa549203989
ricardo85x <ricardo85x@gmail.com>
2021-10-27 20:23:35 +0000

    Deploying to gh-pages from @ ricardo85x/addressbook-dapp@94492d97d7194409d1c5fa97cec22f2a21474a6a 🚀

--- _next/static/fA6YUKO7xzbVDzWaAWZ8W/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-5bb7b7150ac09a67c00b.js","static/chunks/64-c6ab0605915a9740406e.js","static/chunks/pages/index-657a657f18d1ec6eb8d8.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
